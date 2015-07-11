f50da8ca90e6270ce8a42c16a9b29edaaa80b267
Nick Sellen <git@nicksellen.co.uk>
2015-07-11 15:44:44 +0200

    added a bunch of verbs and rules

--- conjugate.js
+++ conjugate.js
@@ -6,6 +6,14 @@ const conjuator = require('./lib/conjugator');
 var infinitive = process.argv[2];
 var results = conjuator(infinitive, ['präsens', 'präteritum', 'k2präsens']);
 var data = [];
+var partizip = conjuator.partizip(infinitive);
+if (partizip) {
+  data.push([infinitive, 'partizip', partizip]);
+}
+var hilfsverb = conjuator.hilfsverb(infinitive);
+if (hilfsverb) {
+  data.push([infinitive, 'hilfsverb', hilfsverb]);
+}
 Object.keys(results).forEach(function(tense){
   var words = results[tense];
   data.push([infinitive, tense, words.ich, words.du, words.es, words.wir, words.ihr]);
--- test/test.js
+++ test/test.js
@@ -1,32 +1,63 @@
 var fs = require('fs');
 var assert = require('assert');
 
-var tests = [];
+var all = {};
 
 fs.readFileSync(__dirname + '/test-verbs.txt', 'utf8').split("\n").map(function(line){
   if (/^#/.test(line)) return;
   if (!line) return;
   var parts = line.split(/\ +/);
-  tests.push({
-    infinitive: parts[0],
-    tense: parts[1],
-    expect: {
-      ich: parts[2],
-      du: parts[3],
-      es: parts[4],
-      wir: parts[5],
-      ihr: parts[6]
-    }
-  });
+  var infinitive = parts[0];
+
+  if (!all[infinitive]) all[infinitive] = [];
+  var tests = all[infinitive];
+
+  if (parts[1] === 'partizip') {
+    tests.push({
+      partizip: parts[2]
+    });
+  } else if (parts[1] === 'hilfsverb') {
+    tests.push({
+      hilfsverb: parts[2]
+    });
+  } else {
+    tests.push({
+      tense: parts[1],
+      expect: {
+        ich: parts[2],
+        du: parts[3],
+        es: parts[4],
+        wir: parts[5],
+        ihr: parts[6]
+      }
+    });
+  }
 });
 
-describe('verb conjugation', function(){
+describe('conjuagtor', function(){
   var conjuagtor = require('../lib/conjugator');
-  tests.forEach(function(test){
-    it('can conjugate ' + test.infinitive + ' into ' + test.tense, function(){
-      var result = conjuagtor(test.infinitive, test.tense);
-      Object.keys(test.expect).forEach(function(pronoun){
-        assert.equal(result[pronoun], test.expect[pronoun]);
+  Object.keys(all).forEach(function(infinitive){
+    var tests = all[infinitive];
+    describe(infinitive, function(){
+      tests.forEach(function(test){
+        if (test.partizip) {
+          it('partizip', function(){
+            assert.equal(conjuagtor.partizip(infinitive), test.partizip);
+          });
+        } else if (test.hilfsverb) {
+          it('hilfsverb', function(){
+            assert.equal(conjuagtor.hilfsverb(infinitive), test.hilfsverb);
+          });
+        } else if (test.tense) {
+          describe(test.tense, function(){
+            var result = conjuagtor(infinitive, test.tense);
+            Object.keys(test.expect).forEach(function(pronoun){
+              it(pronoun, function(){
+                assert.equal(result[pronoun], test.expect[pronoun]);
+              });
+            });
+          });
+        }
       });
     });
   });
