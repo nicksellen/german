7a613d06a1826ca28cd4b291756ec050d713d7be
Nick Sellen <git@nicksellen.co.uk>
2015-07-13 13:37:25 +0200

    can filter list by hilfsverb

--- bin/german-list.js
+++ bin/german-list.js
@@ -3,11 +3,35 @@ const columnify = require('columnify');
 var verbs = require('../lib/verbs');
 
 var app = require('commander');
-app.option('-r, --irregularity', 'Sort by irregularity').parse(process.argv);
+app
+   .option('-r, --irregularity', 'Sort by irregularity')
+   .option('-a, --hilfsverb [verb]', 'Filter by hilfsverb')
+   .parse(process.argv);
 
 var data = [];
 var verbs = Object.keys(verbs).map(function(infinitive){
   return verbs[infinitive];
+});
+
+var filters = [];
+
+if (app.hilfsverb) {
+  if (['sein', 'haben'].indexOf(app.hilfsverb) === -1) {
+    throw new Error('hilfsverb must be sein or haben');
+  }
+  filters.push(function(verb){
+    return verb.hilfsverb === app.hilfsverb;
+  });
+}
+
+verbs = verbs.filter(function(verb){
+  for (var i = 0; i < filters.length; i++) {
+    var filter = filters[i];
+    if (!filter(verb)) {
+      return false;
+    }
+  }
+  return true;
 });
 
 if (app.irregularity) {
