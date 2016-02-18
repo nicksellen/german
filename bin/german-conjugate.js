80b80c9746aa40a13994db3bd3352bf51eb631cd
Nick Sellen <git@nicksellen.co.uk>
2016-02-18 17:42:04 +0000

    show prefix when conjugating

--- bin/german-conjugate.js
+++ bin/german-conjugate.js
@@ -2,22 +2,32 @@
 const columnify = require('columnify');
 
 const conjugator = require('../lib/conjugator');
+const verbs = require('../lib/verbs').verbs;
 
 var infinitive = process.argv[2];
 var results = conjugator(infinitive, ['präsens', 'präteritum', 'perfekt', 'futur1', 'k2präsens', 'k2präteritum']);
 var data = [];
+var verb = verbs[infinitive];
+
 var en = conjugator.en(infinitive);
 if (en) {
   data.push([infinitive, 'en', en]);
 }
+
+if (verb.seperable) {
+  data.push([infinitive, 'prefix', verb.seperable.prefix]);
+}
+
 var partizip = conjugator.partizip(infinitive);
 if (partizip) {
   data.push([infinitive, 'partizip', partizip]);
 }
+
 var hilfsverb = conjugator.hilfsverb(infinitive);
 if (hilfsverb) {
   data.push([infinitive, 'hilfsverb', hilfsverb]);
 }
+
 Object.keys(results).forEach(function(tense){
   var words = results[tense];
   data.push([infinitive, tense, words.ich, words.du, words.es, words.wir, words.ihr]);
